cc4f27d2900534cc5e16d812aef2177f74e7dadd
Tony Duran <yoet92@gmail.com>
2019-08-15 15:53:26 -0300

    opcion de fade on/off, default false

--- main.js
+++ main.js
@@ -3,5 +3,14 @@ var vistas = ["vista1", "vista2", "vista3", "vista4"];
 
 btn.onclick=function(e){
     e.preventDefault();
-    MostrarVista(["vista4", "vista2"], vistas);
+    MostrarVista(["vista4", "vista2"], vistas, {
+        fade: {
+            enabled: false,
+            timeIn: 8,
+            timeOut: 10
+        },
+        callback: function(){
+            alert("this is the callback");
+        }
+    });
 }
--- mostrar-vista.js
+++ mostrar-vista.js
@@ -1,41 +1,66 @@
-function MostrarVista(mostrar, ocultar, callback){
-    function hide(fn){
-        ocultar.forEach(function(e){
-            var hideEle = document.getElementById(e);
-            hideEle.style.opacity = 1;
-            var fadeEffect = setInterval(function() {
-            if (hideEle.style.opacity < 0.1)
-            {
-            hideEle.style.display='none';
-            fn();
-            clearInterval(fadeEffect);
-            }
-            else
-            {
-            hideEle.style.opacity -= 0.1;
-            }
-            }, 20);
-        });
-    }
-    function show(){
-        mostrar.forEach(function(e) {
-            var showEle = document.getElementById(e);
-            showEle.style.opacity = 0;
-            showEle.style.display='block';
-                var i = 0;
-                fadeIn(showEle,i);
-                function fadeIn(showEle,i) {
-                    i = i + 0.05;
-                    seto(showEle,i);
-                    if (i<1){setTimeout(function(){fadeIn(showEle,i);}, 25);}
-                }
-                function seto(el,i)
+'use strict';
+function MostrarVista(mostrar, ocultar, options){
+    options             = options || {};
+    options.callback    = options.callback || new Function;
+    options.fade        = options.fade || {};
+    options.fade.enabled = options.fade.enabled || false;
+    options.fade.timeIn = options.fade.timeIn || 8;
+    options.fade.timeOut = options.fade.timeOut || 10;
+    
+    if(options.fade.enabled){
+        function hide(fn){
+            ocultar.forEach(function(e){
+                var hideEle = document.getElementById(e);
+                hideEle.style.opacity = 1;
+                var fadeEffect = setInterval(function() {
+                if (hideEle.style.opacity < 0.1)
                 {
-                    el.style.opacity = i;
+                hideEle.style.display='none';
+                fn();
+                clearInterval(fadeEffect);
                 }
-        });
+                else
+                {
+                hideEle.style.opacity -= 0.1;
+                }
+                }, options.fade.timeOut);
+            });
+        }
+        function show(){
+            mostrar.forEach(function(e) {
+                var showEle = document.getElementById(e);
+                showEle.style.opacity = 0;
+                showEle.style.display='block';
+                    var i = 0;
+                    fadeIn(showEle,i);
+                    function fadeIn(showEle,i) {
+                        i = i + 0.05;
+                        seto(showEle,i);
+                        if (i<1){setTimeout(function(){fadeIn(showEle,i);}, options.fade.timeIn);}
+                    }
+                    function seto(el,i)
+                    {
+                        el.style.opacity = i;
+                    }
+            });
+        }
+        hide(show);
+    }else{
+        function hide(fn) {
+            ocultar.forEach(function (e) {
+                var hideEle = document.getElementById(e);
+                hideEle.style.display = 'none';
+            });
+            fn();
+        }
+        function show() {
+            mostrar.forEach(function (e) {
+                var showEle = document.getElementById(e);
+                showEle.style.display = 'block';
+            });
+        }
+        hide(show);
     }
-    hide(show);
-    callback = callback || new Function;
-    callback();
+    
+    options.callback();
 }
